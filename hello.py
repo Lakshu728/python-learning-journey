007d38da647b16d34c8a2ac3be95c049847e9fa4
Lakshita287 <141507472+Lakshu728@users.noreply.github.com>
2026-04-01 19:23:01 +0530

    First Program in python

---
+++ hello.py
@@ -0,0 +1,59 @@
+print(" hello world")
+a=5
+b=10
+print(type(a))
+print(type(b))
+d='lakshita'
+print(type(d))
+
+#calculate the sum
+c=a+b
+print("the sum of a and b is",c)
+#calculate the difference
+d=a-b
+print("the difference of a and b is",d)
+#calculate the product
+e=a*b     
+print("the product of a and b is",e)
+#calculate the quotient
+f=a/b
+print("the quotient of a and b is",f)   
+#calculate the square of a
+g=a**2
+print("the square of a is",g)
+#calculate the square of b      
+h=b**2
+print("the square of b is",h)
+
+#convert int to float
+i=float(a)
+print("the float value of a is",i)
+#convert float into int
+f=10.10
+j=int(f)
+print("the int value of f is",j)
+
+
+
+#calculate the area of a circle
+import math
+radius=5
+area=math.pi*radius**2
+print("the area of the circle is",area)
+
+
+
+#calculate th area of a rectangle
+length=15
+width=20
+area_rectangle=length*width
+print("the area of the rectangle is",area_rectangle)
+
+
+#calculate the area of a triangle
+base=10 
+height=5
+area_triangle=0.5*base*height
+print("the area of the triangle is",area_triangle)
+
+
